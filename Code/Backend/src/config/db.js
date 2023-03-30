059c63d90f2b7c3025f8de88b6155872a9b79611
AlexandraElena07 <alexandraelenasirbulescu@gmail.com>
2023-03-30 20:53:30 +0300

    TC-17 <added backend setup>

---
+++ Code/Backend/src/config/db.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const { MONGODB_URI } = process.env;
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            setNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("DB Connected");
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+connectToDB();
